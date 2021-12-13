5b6086671ed65db3e4cb06c6f687eea92a31f2dd
Hugo MANCINI <hgmancini@gmail.com>
2021-12-13 10:24:53 +0100

    first -commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  validates :first_name, presence: true, uniqueness: { scope: :last_name }
+  validates :last_name, presence: true
+end
